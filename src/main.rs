23f67fb66c7b554c8f4efc5e529000fb43b8e92a
mdc <galacticwingcommander@gmail.com>
2024-05-12 16:23:06 +0800

    Basic index endpoint rendered with title variable. Not connecting to css file yet.

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,15 @@ use std::fs::File;
 use std::io::prelude::*;
 use std::sync::Mutex;
 use serde_yaml;
+use actix_web::{HttpServer, App, web, HttpResponse, Responder};
+use tera::{Tera, Context};
 
 use crate::settings::Settings;
 
 pub mod settings;
 
 // Create a global variable for applications settings.
-// This was available in other files.
+// This will be available in other files.
 lazy_static! {
     static ref SETTINGS: Mutex<Settings> = {
         // Read YAML settings file.
@@ -23,17 +25,38 @@ lazy_static! {
 
         // Deserialize YAML into Settings struct.
         let settings: Settings = serde_yaml::from_str(&contents).expect("Unable to parse YAML");
-        info!("Application settings loaded, version: {}", settings.settings_version);
         Mutex::new(settings)
     };
 }
 
-// Steganoraphy mainline.
-fn main() {
-    // Set up application logging.
-    // Configuration held in log4rs.yml .
+async fn index(tera: web::Data<Tera>) -> impl Responder {
+    let mut data = Context::new();
+    let settings: Settings = SETTINGS.lock().unwrap().clone();
+    data.insert("title", &settings.program_name);
+    data.insert("name", &settings.program_devs);
+
+    let rendered = tera.render("index.html", &data).unwrap();
+    HttpResponse::Ok().body(rendered)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    // Logging onfiguration held in log4rs.yml .
     log4rs::init_file("log4rs.yml", Default::default()).unwrap();
 
     // Get application metadata to include in initial logging.
     info!("Application started, version: {}", env!("CARGO_PKG_VERSION"));
+
+    // Load the starting template file.
+    HttpServer::new(|| {
+        let tera = Tera::new("templates/**/*").unwrap();
+        App::new()
+            .data(tera)
+            .route("/", web::get().to(index))
+    })
+
+    // Start the web application.
+    .bind("127.0.0.1:8000")?
+    .run()
+    .await
 }
