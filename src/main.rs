5167e3a8ab87478325172dc67d1a20ed94d51245
mdc <galacticwingcommander@gmail.com>
2024-05-25 16:37:01 +0800

    Basic image browsing and upload working. Css styling still a problem for now.

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,17 @@
 
 use log::info;
 use log4rs;
+use futures::{StreamExt, TryStreamExt};
 use lazy_static::lazy_static;
 use std::fs::File;
+use std::io::Write;
 use std::io::prelude::*;
-use std::sync::Mutex;
-use actix_files as fs;
-use actix_web::{get, App, HttpServer, Responder, HttpResponse};
+use std::sync::{Arc, Mutex};
+use sanitize_filename::sanitize;
+// use actix_files as fs;
+use actix_multipart::Multipart;
+
+use actix_web::{get, post, web, App, HttpServer, HttpResponse, Responder};
 
 use crate::settings::Settings;
 use crate::steg::Steganography;
@@ -35,6 +40,39 @@ async fn intro() -> impl Responder {
     HttpResponse::Ok().content_type("text/html").body(include_str!("../static/index.html"))
 }
 
+#[post("/upload")]
+async fn upload(mut payload: Multipart, steg: web::Data<Arc<Mutex<Steganography>>>,) -> impl Responder {
+    let steg = steg.clone();
+    while let Ok(Some(mut field)) = payload.try_next().await {
+        let content_disposition = field.content_disposition();
+        if let Some(filename) = content_disposition.get_filename() {
+            let filepath = format!("./tmp/{}", sanitize(&filename));
+            let filepath_clone = filepath.clone(); // Clone filepath for later use
+
+            // File::create is a blocking operation, use threadpool.
+            let mut f = web::block(move || File::create(filepath)).await.unwrap().unwrap();
+
+            // Field in turn is stream of *Bytes* object.
+            while let Some(chunk) = field.next().await {
+                let data = chunk.unwrap();
+                // Filesystem operations are blocking, we have to use threadpool.
+                f = web::block(move || {
+                    let mut file = f;
+                    file.write_all(&data).map(|_| file)
+                }).await.unwrap().unwrap();
+            }
+
+            // Process the uploaded file with Steganography instance
+            let steg = steg.lock().unwrap();
+            // <MDC> Test, remove later.
+            println!("Test that I can access steg... {}", steg.img_to_proc);
+            println!("Test that I can access filename... {:?}", filepath_clone);
+        }
+    }
+
+    HttpResponse::Ok().body("File uploaded successfully")
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     // Logging configuration held in log4rs.yml .
@@ -46,13 +84,15 @@ async fn main() -> std::io::Result<()> {
 
     // Instatiate a steganography struct.
     // Call init method to initialise struct.
-    let _img_steg = Steganography::init();
+    let img_steg = Arc::new(Mutex::new(Steganography::init()));
 
     // Create and start web service.
-    HttpServer::new(|| {
+    HttpServer::new(move || {  // Use the `move` keyword to capture `img_steg`
         App::new()
+            .app_data(web::Data::new(img_steg.clone()))
             .service(intro)
-            .service(fs::Files::new("/static", "./static").show_files_listing())
+            .service(upload)
+            .service(actix_files::Files::new("/static", "./static").show_files_listing())
     })
     .bind("127.0.0.1:8080")?
     .run()
