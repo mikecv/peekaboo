34aa35d0cb659b1db0096c68a3014c1d09800d2f
mdc <galacticwingcommander@gmail.com>
2024-08-25 17:38:08 +0800

    Added eye to password entry to reveal password.

--- static/index-scripts.js
+++ static/index-scripts.js
@@ -584,3 +584,19 @@ function toggleBorder(imgElement, borderOn) {
         imgElement.classList.remove('border-on');
     }
 }
+
+// Function for revealing passwoed entry locally.
+function togglePasswordVisibility(inputId, iconId) {
+    const passwordInput = document.getElementById(inputId);
+    const eyeIcon = document.getElementById(iconId);
+    
+    if (passwordInput.type === "password") {
+        passwordInput.type = "text";
+        eyeIcon.classList.remove('fa-eye');
+        eyeIcon.classList.add('fa-eye-slash');
+    } else {
+        passwordInput.type = "password";
+        eyeIcon.classList.remove('fa-eye-slash');
+        eyeIcon.classList.add('fa-eye');
+    }
+}
